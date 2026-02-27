8e587892b09251c04b5b1b8d54a8cb9a80b62e1e
AUGUSTO PEREIRA TEIXEIRA <unifateixeira@feinet.fei.edu.br>
2026-02-27 18:42:27 -0300

    arrumei colisao e player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -21,12 +21,12 @@ public class Player : MonoBehaviour
         Vector2 dir = mousePos - playerPos;
         Vector2 vel = rb2d.linearVelocity;
 
-        if (dir.x <= 0.01f && dir.y <= 0.01f)
+        if ((dir.x <= 0.15f && dir.y <= 0.15f && dir.x >= -0.15f && dir.y >= -0.15f))
         {
             rb2d.linearVelocity = Vector2.zero;
-            pos.x = mousePos.x;
-            pos.y = mousePos.y;
-            checkBounds(pos, vel);
+            //pos.x = mousePos.x;
+            //pos.y = mousePos.y;
+            checkBounds(pos);
             return;
         }
         dir.Normalize();
@@ -37,41 +37,46 @@ public class Player : MonoBehaviour
 
         vel.x = forceVec.x;
         vel.y = forceVec.y;
-        pos.x = mousePos.x;
-        pos.y = mousePos.y;
+        //pos.x = mousePos.x;
+        //pos.y = mousePos.y;
 
-        checkBounds(pos, vel);
-
+        checkBounds(pos);
+        rb2d.linearVelocity = vel;
 
 
     }
 
-    void checkBounds(Vector2 pos, Vector2 vel)
+    void checkBounds(Vector2 pos)
     {
+        bool is_out = false;
         if (pos.x > 4.4f)
         {
             pos.x = 4.4f;
-            vel.x = 0;
+            is_out = true;
+            //vel.x = 0;
         }
         else if (pos.x < -4.4f)
         {
             pos.x = -4.4f;
-            vel.x = 0;
+            is_out = true;
+            //vel.x = 0;
         }
 
         // Limites Verticais (Eixo Y)
         if (pos.y > -1f)
         {
             pos.y = -1f;
-            vel.y = 0;
+            is_out = true;
+            //vel.y = 0;
         }
         else if (pos.y < -7f)
         {
             pos.y = -7f;
-            vel.y = 0;
+            is_out = true;
+            //vel.y = 0;
         }
-        transform.position = pos;
-        rb2d.linearVelocity = vel;
+        if(is_out) transform.position = pos;
+        //rb2d.linearVelocity = vel;
     }
 
     void OnCollisionEnter2D(Collision2D coll)
